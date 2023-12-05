bfb2551d08c7ab9dbcaa48e50aa853b0987b3298
Rishabh Rawat <rishabh.rawat@smallcase.com>
2023-12-05 22:48:46 +0530

    fix: add config for postgres user password

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     user: process.env.DB_USER || 'myuser',
     host: process.env.DB_HOST || '127.0.0.1',
     database: process.env.DB_NAME || 'postgres',
+    password: process.env.DB_USER_PASSWORD || 'password',
     port: 5432,
   }
 }
